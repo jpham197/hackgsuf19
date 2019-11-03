41f3c57a2586ecc80f59d1a8939bac3557d7e273
jpham197 <jpham197@gmail.com>
2019-11-03 01:37:23 -0400

    logic and basic display of orders functional, no duplicates and stuff

--- src/app/kitchen/queue/queue.component.ts
+++ src/app/kitchen/queue/queue.component.ts
@@ -3,23 +3,53 @@ import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
 import { OrdersService } from 'src/app/services/orders.service';
 import { MatButtonModule } from "@angular/material";
+import { element } from '@angular/core/src/render3';
+import { config } from '../../services/config'
 
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
   styleUrls: ['./queue.component.css']
 })
+
 export class QueueComponent implements OnInit, OnDestroy {
 
-  constructor(private orders: OrdersService) { }
+  constructor(private os: OrdersService) { }
 
   itemsObs$;
   items: any = [
   ]
 
+  tlogIds = [];
+  orders = [];
+  pastOrders = [];
+
   ngOnInit() {
-    this.call();
-    this.orders.getTransactions();
+    setInterval(() =>{
+      this.os.getTransactions()
+        .subscribe(data => {
+          data["pageContent"].forEach(element => {
+            this.tlogIds.push(element.tlogId);
+          })
+          this.tlogIds.forEach(id => {
+            this.os.getOrders(id).subscribe(element => {
+              // this.orders.push(order);
+              let order = element["tlog"]["items"];
+              order.forEach(elem => {
+                if (!this.ordersContains(elem.id)) {
+                  this.orders.push(elem);
+                  this.pastOrders.push(elem.id)
+                }
+              });
+            })
+          });
+        });
+    }, 1000);
+
+    //apparently this waits until the data is populated??
+    setTimeout(() => {
+      // console.log(this.orders)
+    }, 1500);
   }
 
   @HostListener('document:keyup', ['$event'])
@@ -31,20 +61,28 @@ export class QueueComponent implements OnInit, OnDestroy {
   }
 
   call(): void {
-    this.itemsObs$ = this.orders.getItems().subscribe((result) => {
+    this.itemsObs$ = this.os.getItems().subscribe((result) => {
       this.items = result;
-      // console.log(this.items);
-    })
+    });
   }
 
   dequeue(): void {
     this.items.shift();
-    console.log(this.items);
+
+    this.orders.shift();
+  }
+
+  ordersContains(id) {
+    for (let i = 0; i < this.pastOrders.length; i++) {
+      if (this.pastOrders[i] === id) {
+        return true;
+      }
+    }
+    return false;
   }
 
   ngOnDestroy() {
     this.itemsObs$.unsubscribe();
   }
-
 
 }
